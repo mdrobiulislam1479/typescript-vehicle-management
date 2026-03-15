33b0730287f34115af5cca7255721ce6202ad08d
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-03-15 22:48:07 +0600

    created car class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 interface Vehicle {
   brand: string;
-  speed: string;
+  speed: number;
   move(): void;
+}
+
+class Car implements Vehicle {
+  constructor(
+    public brand: string,
+    public speed: number,
+  ) {}
+  move(): void {
+    console.log(`The ${this.brand} car is moving at ${this.speed} km/h.`);
+  }
 }
